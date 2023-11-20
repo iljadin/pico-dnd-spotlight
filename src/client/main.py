8268381680bfefd9e54d40385280534a3f3801e4
iljadin <43754460+iljadin@users.noreply.github.com>
2023-11-20 01:01:47 +0100

    Added libraries from previous projects
    Bluetooth, servo and ws2812 as well as threading in main

--- src/client/main.py
+++ src/client/main.py
@@ -1,3 +1,23 @@
 #Name:      main.py
 #Date:      18.11.2023
 #Author:    Iljadin Manurung
+import _thread
+
+
+
+def system_threading():
+    second_thread = _thread.start_new_thread(core1_thread, ())
+    core0_thread()
+    pass
+
+def core0_thread():
+    pass
+
+
+def core1_thread():
+    pass
+
+
+if __name__ == "__main__":
+    system_threading()
+    print("running")
